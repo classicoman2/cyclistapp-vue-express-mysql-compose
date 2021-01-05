b666ad183ed2796f52a8ed6042731f575a9bfb4c
Toni <aamengual@cifpfbmoll.eu>
2021-01-05 20:48:38 +0100

    loadData.js

--- server/loadData/loadData.js
+++ server/loadData/loadData.js
@@ -2,20 +2,28 @@
   Node.js script per carregar a base de dades MongoDB les dades dels equips i dels ciclistes
   */
 
-  //Carrega variables entorn de fitxer .env
+//Carrega variables entorn de fitxer .env
+
+const fs = require("fs");
 
 var dotenv = require("dotenv");
 dotenv.config();
 
-console.log(process.env)
-
+//console.log(process.env);
 
 // Connectar a Mongo
 
-
-
 // Crear collection i crear database
 
+// Llegir fitxers de teams i cyclists i carregar-los dins la BDD
 
-// Llegir fitxers json i carregar-los dins la BDD
+const testFolder = "./data/json/";
 
+fs.readdirSync(testFolder).forEach((file) => {
+    console.log(file)
+
+    let rawdata = fs.readFileSync(testFolder + file);
+  let teamsData = JSON.parse(rawdata);
+  console.log(teamsData[0].ciclistes[0]);
+  
+});
